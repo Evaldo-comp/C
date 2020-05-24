5731cbaf9a726e82bc9f1836ff709b6a359f7d6c
3v4ld0 <evaldocomp@gmail.com>
2020-05-24 03:29:36 -0300

    upodate

---
+++ Lacos_repeticao/pratica_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+//Faça um programa que imprima os números pares de 1- a 20 utilizando os laços While, Do While ou For
+
+void main(){
+
+int count = 20;
+
+for (int i = 10; i <= count; i = i+2){
+	if (i % 2 == 0){
+	printf("%d\n", i );	
+	}
+}
+	
+}
+
+
+
+
