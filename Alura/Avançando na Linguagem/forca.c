653f22a3261c045f61d72b2e2d8a3b7de13a5f4c
Evaldo-comp <evaldocomp@gmail.com>
2020-12-08 04:36:44 -0300

    upadate - forca alura

---
+++ Alura/Avançando na Linguagem/forca.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char palavrasecreta[20];
+
+    sprintf(palavrasecreta, "Melancia");
+
+    int acertou = 0;
+    int enforcou = 0;
+
+    char chutes[26];
+    int tentativas = 0;
+
+    do {
+        
+        for (int i; i < strlen(palavrasecreta); i++){
+            int achou = 0;
+            printf("Estou vendo a letra secreta %d %c\n", i, palavrasecreta[i]);
+            for(int j = 0; j < tentativas; j++){
+                printf("-> Chute %d %c\n",j,  chutes[j]);
+                if (chutes[j] == palavrasecreta[i]){
+                    printf("-----> Chute Correto\n");
+                    achou = 1;
+                    break;
+                }
+            } 
+            if (achou){
+                printf("%c ", palavrasecreta[i]);
+            }else{
+                printf("_ ");
+            }
+            
+        }
+        printf("\n");
+
+        char chute;
+        printf("Qual é a letra? ");
+        scanf(" %c", &chute);
+
+        chutes[tentativas] = chute;
+        tentativas++;
+
+           
+        
+    }while (!acertou && !enforcou);
+    
+}
